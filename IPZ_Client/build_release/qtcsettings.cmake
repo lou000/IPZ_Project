34d3b0b9480b808baabf30ee4be0d476b4f454b5
lou000 <lou000@users.noreply.github.com>
2021-03-10 17:08:39 +0100

    Initial commit

---
+++ IPZ_Client/build_release/qtcsettings.cmake
@@ -0,0 +1,7 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.16.27023/bin/HostX86/x64/cl.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.16.27023/bin/HostX86/x64/cl.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/5.15.0/msvc2019_64" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/5.15.0/msvc2019_64/bin/qmake.exe" CACHE "STRING" "" FORCE)
