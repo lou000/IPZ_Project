c60e387a7473cb534d0d2f609a743aba4cfc6ee7
lou000 <44552514+lou000@users.noreply.github.com>
2021-04-16 02:45:12 +0200

    tests

--- IPZ_Client/src/main.cpp
+++ IPZ_Client/src/main.cpp
@@ -151,35 +151,35 @@ int main(void)
                 Renderer::DrawQuad(pos, {0.8f, 0.8f}, tileText);
         }
 
-#if 0
+#if 1
         float w = 0.09f;
         float z = w/2;
         float x = w/2;
         vec4 colorStart = {0.969, 0.588, 0.498,1.0f};
         vec4 colorEnd = {0.255, 0.263, 0.478,1};
-        Renderer::DrawQuad({0,0,0}, {4, 4}, vec4(0.090, 0.059, 0.286, 1));
+        Renderer::DrawQuad({0,0.001,0}, {4, 4}, vec4(0.090, 0.059, 0.286, 1));
         while(x-w/2<4)
         {
             while(z-w/2<4)
             {
                 float mixA = clamp((float)(8-(z+x))/8+0.25f, 0.f, 1.0f);
-                Renderer::DrawQuad({x-2, 0,z-2}, {w, w}, mix(colorStart, colorEnd, mixA));
+                Renderer::DrawQuad({x-2, 0.002,z-2}, {w, w}, mix(colorStart, colorEnd, mixA));
                 z+=w+0.01f;
             }
             z = w/2;
             x+=w+0.01f;
         }
 
+        float y = 0.0001;
         for(auto& pos : randomPos)
         {
             pos.x += (float)rndInt(-2, 1)/200;;
             pos.y += (float)rndInt(-2, 1)/200;
             pos = mod(pos, {6,6});
-            Renderer::DrawQuad({pos.x-3, 0, pos.y-3}, {0.3f, 0.3f}, texture2);
+            Renderer::DrawQuad({pos.x-3, y, pos.y-3}, {0.3f, 0.3f}, texture2);
+            y+=0.0001;
         }
-        Renderer::DrawQuad({0,0,0}, {2, 2}, texture);
-
-        Renderer::DrawQuad({0,0,0}, {0.05f, 0.05f},{1,1,1,1});
+        Renderer::DrawQuad({0,0.1,0}, {2, 2}, texture);
 #endif
 
         Renderer::end();
