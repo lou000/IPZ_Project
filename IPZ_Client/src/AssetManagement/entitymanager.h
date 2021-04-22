3bdb0af158ac34c7833617f9b2b9c1e1c444a143
lou000 <44552514+lou000@users.noreply.github.com>
2021-04-22 21:45:59 +0200

    prototyping

--- IPZ_Client/src/AssetManagement/entitymanager.h
+++ IPZ_Client/src/AssetManagement/entitymanager.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "../Core/utilities.h"
+#define MAX_ENTITIES 2000
 // unordered_map<int, weak?_pointer>
 // large preallocated memory block
 // deleted entities marked as free memory
@@ -9,9 +11,30 @@
 // additional unordered_map<int, vector<ptr>> to quickly get entities by type?
 // get weak_pointer when we retrieve entities?
 // this might get complicated and not be very fast
+using namespace glm;
+
+class Entity
+{
+public:
+    virtual void onCreate() = 0;
+    virtual void onUpdate(float dt) = 0;
+    virtual void onDestroy() = 0;
+
+private:
+    uint id;
+    vec3 pos;
+    quat rotation;
+};
+
 class EntityManager
 {
 public:
     EntityManager();
+    std::weak_ptr<Entity> getEntity(uint id);
+
+private:
+    byte* data;
 };
+
+
 
