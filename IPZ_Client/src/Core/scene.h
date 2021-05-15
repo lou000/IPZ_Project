42a798d660a331aafd99a4e38d5af94d99b4afb1
lou000 <44552514+lou000@users.noreply.github.com>
2021-05-16 01:35:33 +0200

    base Scene class

---
+++ IPZ_Client/src/Core/scene.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+
+// For now this is base class that represents the "game"
+class Scene
+{
+public:
+    Scene(){};
+    virtual ~Scene(){};
+    virtual void onUpdate(float dt) = 0;
+};
+
