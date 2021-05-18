568e44b349d54fdfc348c5415b660dfbf61f33e8
lou000 <44552514+lou000@users.noreply.github.com>
2021-05-18 04:47:08 +0200

    added draw point

--- IPZ_Client/src/Renderer/batchrenderer.h
+++ IPZ_Client/src/Renderer/batchrenderer.h
@@ -46,12 +46,14 @@ public:
     {getInstance().x_drawQuad(pos, size, texture, tilingFactor, tintColor);}
     static void drawQuad(const vec3 &pos, const vec2 &size, const vec4 &tintColor)
     {getInstance().x_drawQuad(pos, size, tintColor);}
+    static void drawLine(const vec3& posStart, const vec3& posEnd, float width, const vec4& color)
+    {getInstance().x_drawLine(posStart, posEnd, width, color);}
+    static void drawPoint(const vec3& pos, float lWidth = 0.01f, float lLen = 0.1f, const vec4& color = {1,1,1,1})
+    {getInstance().x_drawPoint(pos, lWidth, lLen, color);}
+
     static void drawQuad(const vec2 &pos, const vec2 &size, const vec4 &tintColor)
     {getInstance().x_drawQuad(pos, size, tintColor);}
-
     static void drawLine(const vec2& posStart, const vec2& posEnd, float width, const vec4& color)
-    {getInstance().x_drawLine(posStart, posEnd, width, color);}
-    static void drawLine(const vec3& posStart, const vec3& posEnd, float width, const vec4& color)
     {getInstance().x_drawLine(posStart, posEnd, width, color);}
     static void drawCircle(const vec2& pos, float radius, int triangles, const vec4& color)
     {getInstance().x_drawCircle(pos, radius, triangles, color);}
@@ -94,10 +96,11 @@ private:
     void x_drawQuad(const vec3 &pos, const vec2 &size, const std::shared_ptr<Texture> &texture,
                    float tilingFactor, const vec4 &tintColor);
     void x_drawQuad(const vec3 &pos, const vec2 &size, const vec4 &tintColor);
-    void x_drawQuad(const vec2 &pos, const vec2 &size, const vec4 &tintColor);
-
-    void x_drawLine(const vec2& posStart, const vec2& posEnd, float width, const vec4& color);
     void x_drawLine(const vec3& posStart, const vec3& posEnd, float width, const vec4& color);
+    void x_drawPoint(const vec3& pos, float lWidth, float lLen, const vec4& color);
+
+    void x_drawQuad(const vec2 &pos, const vec2 &size, const vec4 &tintColor);
+    void x_drawLine(const vec2& posStart, const vec2& posEnd, float width, const vec4& color);
     void x_drawCircle(const vec2& pos, float radius, int triangles, const vec4& color);
 
     void x_setShader(std::shared_ptr<Shader> shader){m_currentShader = shader;}
