5a08f006d6ffefa9f8bbf3b0dcac9ed086009ce8
lou000 <44552514+lou000@users.noreply.github.com>
2021-05-18 04:48:53 +0200

    asd

--- IPZ_Client/src/Renderer/meshrenderer.cpp
+++ IPZ_Client/src/Renderer/meshrenderer.cpp
@@ -37,17 +37,16 @@ void MeshRenderer::x_drawMesh(const vec3& pos, const vec3& size, const std::shar
     x_drawMesh(model, mesh, color);
 }
 
-void MeshRenderer::x_generateCubeSphere(const vec3 &pos, int vPerEdge, float radius, const vec4 &color)
+ColoredMesh MeshRenderer::generateCubeSphere(int vPerEdge)
 {
     ASSERT(vPerEdge>=2);
 
-    // Vertices
     float offset = (float)1/(vPerEdge-1);
     int cornerVertices = 8;
     int edgeVertices = (vPerEdge-2) * 12;
     int faceVertices = (vPerEdge-2)*(vPerEdge-2)*6;
     size_t vCount = cornerVertices + edgeVertices + faceVertices;
-    vec3* posVerts = (vec3*)alloca(vCount*sizeof(vec3));
+    vec3* posVerts = (vec3*)malloc(vCount*sizeof(vec3));
 
     size_t v = 0;
     for(int y = 0; y<vPerEdge; y++)
@@ -69,16 +68,112 @@ void MeshRenderer::x_generateCubeSphere(const vec3 &pos, int vPerEdge, float rad
             posVerts[v++] = vec3(-0.5, -0.5+y*offset, -0.5+i*offset);
     }
     // top face
+    int topStart = v;
     for (int z = 1; z < vPerEdge-1; z++)
         for (int x = 1; x < vPerEdge-1; x++)
             posVerts[v++] = vec3(-0.5+x*offset,  0.5, 0.5-z*offset);
 
     // bottom face
+    int bottomStart = v;
     for (int z = 1; z < vPerEdge-1; z++)
         for (int x = 1; x < vPerEdge-1; x++)
             posVerts[v++] = vec3(-0.5+x*offset, -0.5, 0.5-z*offset);
 
+    // INDICES
+    int iCount = 6*(vPerEdge-1)*(vPerEdge-1)*2*3; //six faces of (vPerEdge-1)^2 quads 2 tris per quad 3 indices per tri
+    uint16* indices = (uint16*)malloc(iCount*sizeof(uint16));
+    size_t ind = 0;
 
 
+    auto indexQuad = [&](uint16 p0, uint16 p1, uint16 p2, uint16 p3){
+        indices[ind+0] = p0;
+        indices[ind+1] = p1;
+        indices[ind+2] = p3;
+        indices[ind+3] = p1;
+        indices[ind+4] = p3;
+        indices[ind+5] = p2;
+        ind+=6;
+    };
 
+    // sides
+    int loop = (vPerEdge-2) * 4 + 4;
+    for(int y=0; y<vPerEdge-1; y++)
+    {
+        int offset = y*loop;
+        for(int i=0; i<loop-1; i++)
+            indexQuad(offset+i,offset+i+1,offset+i+loop+1, offset+i+loop);
+        indexQuad(offset+loop-1, offset+0, offset+loop, offset+loop+loop-1);
+    }
+
+    // top
+    int bottomLeft  = loop*(vPerEdge-1);
+    int bottomRight = loop*(vPerEdge-1)+vPerEdge-1;
+    int topRight    = loop*(vPerEdge-1)+(vPerEdge-1)*2;
+    int topLeft     = loop*(vPerEdge-1)+(vPerEdge-1)*3;
+
+    int topRowW = vPerEdge-2;
+    // top - bottom row
+    indexQuad(bottomLeft, bottomLeft+1, topStart, topLeft+vPerEdge-2);
+    for(int i=0; i<topRowW-1; i++)
+        indexQuad(bottomLeft+i+1, bottomLeft+i+2, topStart+i+1, topStart+i);
+    indexQuad(bottomRight-1, bottomRight, bottomRight+1, topStart+topRowW-1);
+
+    // top - middle rows
+    for(int i=0; i<topRowW-1; i++)
+    {
+        // good thing i have to write this only once...
+        indexQuad(topLeft+vPerEdge-i-2, topStart+topRowW*i, topStart+topRowW*(i+1), topLeft+topRowW-i-1);
+        for(int j=0; j<topRowW-1; j++)
+            indexQuad(topStart+topRowW*i+j, topStart+topRowW*i+j+1, topStart+topRowW*(i+1)+j+1, topStart+topRowW*(i+1)+j);
+        indexQuad(topStart+topRowW+topRowW*i-1, bottomRight+1+i, bottomRight+1+i+1, topStart+topRowW+topRowW*(i+1)-1);
+    }
+
+    // top - top row
+    indexQuad(topLeft+1, topStart+topRowW*(topRowW-1), topLeft-1, topLeft);
+    for(int i=0; i<topRowW-1; i++)
+        indexQuad(topStart+topRowW*(topRowW-1)+i, topStart+topRowW*(topRowW-1)+i+1, topLeft-i-2, topLeft-i-1);
+    indexQuad(topStart+topRowW*topRowW-1, topRight-1, topRight, topRight+1);
+
+
+    // bottom
+    auto indexQuadInv = [&](uint16 p0, uint16 p1, uint16 p2, uint16 p3){
+        indices[ind+0] = p3;
+        indices[ind+1] = p2;
+        indices[ind+2] = p0;
+        indices[ind+3] = p2;
+        indices[ind+4] = p0;
+        indices[ind+5] = p1;
+        ind+=6;
+    };
+    bottomLeft  = 0;
+    bottomRight = (vPerEdge-1);
+    topRight    = 2*(vPerEdge-1);
+    topLeft     = 3*(vPerEdge-1);
+
+
+    // bottom - bottom row
+    indexQuadInv(bottomLeft, bottomLeft+1, bottomStart, topLeft+topRowW);
+    for(int i=0; i<topRowW-1; i++)
+        indexQuadInv(bottomLeft+i+1, bottomLeft+i+2, bottomStart+i+1, bottomStart+i);
+    indexQuadInv(bottomRight-1, bottomRight, bottomRight+1, bottomStart+topRowW-1);
+
+    // bottom - middle rows
+    for(int i=0; i<topRowW-1; i++)
+    {
+        // good thing i have to write this only once...
+        indexQuadInv(topLeft+vPerEdge-i-2, bottomStart+topRowW*i, bottomStart+topRowW*(i+1), topLeft+topRowW-i-1);
+        for(int j=0; j<topRowW-1; j++)
+            indexQuadInv(bottomStart+topRowW*i+j, bottomStart+topRowW*i+j+1, bottomStart+topRowW*(i+1)+j+1, bottomStart+topRowW*(i+1)+j);
+        indexQuadInv(bottomStart+topRowW+topRowW*i-1, bottomRight+1+i, bottomRight+1+i+1, bottomStart+topRowW+topRowW*(i+1)-1);
+    }
+
+    // top - top row
+    indexQuadInv(topLeft+1, bottomStart+topRowW*(topRowW-1), topLeft-1, topLeft);
+    for(int i=0; i<topRowW-1; i++)
+        indexQuadInv(bottomStart+topRowW*(topRowW-1)+i, bottomStart+topRowW*(topRowW-1)+i+1, topLeft-i-2, topLeft-i-1);
+    indexQuadInv(bottomStart+topRowW*topRowW-1, topRight-1, topRight, topRight+1);
+
+    // do the spherification and normals
+
+    return ColoredMesh{};
 }
--- IPZ_Client/src/Renderer/meshrenderer.h
+++ IPZ_Client/src/Renderer/meshrenderer.h
@@ -12,17 +12,16 @@ using namespace glm;
 // do instanced rendering, setup queu of meshes and models, render them at "end"
 // https://learnopengl.com/Advanced-OpenGL/Instancing
 // https://www.informit.com/articles/article.aspx?p=2033340&seqNum=5 normal matrix from model matrix!!
+struct ColoredMesh{
+    vec3* posV;
+    vec3* normals;
+    vec4* colors;
+    uint16* indices;
+    uint indexCount;
+    uint16 vertexCount;
+};
 class MeshRenderer
 {
-    struct ColoredMesh{
-        vec3 posV;
-        vec3 normals;
-        vec4 colors;
-        uint16* indices;
-        uint indexCount;
-        uint16 vertexCount;
-    };
-
     MeshRenderer() = default;
     static MeshRenderer& getInstance(){
         static MeshRenderer instance;
@@ -41,7 +40,7 @@ public:
     {getInstance().x_drawMesh(model, mesh, color);}
     static void drawMesh(const vec3 &pos, const vec3 &size, const std::shared_ptr<MeshFile>& mesh, const vec4& color)
     {getInstance().x_drawMesh(pos, size, mesh, color);}
-    static std::shared_ptr<VertexArray> generateCubeSphere(int vPerEdge);
+    static ColoredMesh generateCubeSphere(int vPerEdge);
 
 
 private:
