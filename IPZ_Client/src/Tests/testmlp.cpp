c9c3f733e0d1f85f42434c0e1bf14411cca6794e
lou000 <44552514+lou000@users.noreply.github.com>
2021-05-21 18:46:56 +0200

    split graph functionality to separate class

--- IPZ_Client/src/Tests/testmlp.cpp
+++ IPZ_Client/src/Tests/testmlp.cpp
@@ -5,33 +5,22 @@ TestMLP::TestMLP()
 {
     BatchRenderer::setShader(AssetManager::getShader("batch"));
     MeshRenderer::setShader(AssetManager::getShader("mesh"));
-    sphere = MeshRenderer::createCubeSphere(4);
+
     points = (vec3*)malloc(1000*sizeof(vec3));
     vec2 range = {0.f, glm::pi<float>()};
+    graph.setMesh(meshX, meshX, false);
 
     // points
-    for(int i=0; i<1000; i++)
+    for(uint i=0; i<pointCount; i++)
     {
         auto& p = points[i];
         p.x = glm::linearRand(range.x, range.y);
         p.z = glm::linearRand(range.x, range.y);
-        p.y = mapToRange({-1,1}, {0, 10}, cos(p.x*p.z)*cos(2*p.x));
-
-        p.x = mapToRange(range, {0,10}, p.x);
-        p.z = mapToRange(range, {0,10}, p.z);
+        p.y = cos(p.x*p.z)*cos(2*p.x);
     }
 
-    // mesh grid
-    meshGrid = (vec3*)malloc(meshX*meshX*sizeof(vec3));
+    graph.setPoints(points, pointCount);
 
-    float step = (float)10/(meshX-1);
-    for(uint i=0; i<meshX; i++)
-        for(uint j=0; j<meshX; j++)
-        {
-            auto& p = meshGrid[i*meshX+j];
-            p.x = j*step;
-            p.z = i*step;
-        }
 }
 
 void TestMLP::onUpdate(float dt)
@@ -40,73 +29,134 @@ void TestMLP::onUpdate(float dt)
     GraphicsContext::getCamera()->setFocusPoint({5,5,5});
     accum+=dt/2;
 
+    graph.updateMesh([=](const vec2& p){
+        return cos(p.x*p.y+accum)*cos(2*p.x+accum);
+    });
+    graph.draw({0,0,0});
+
+}
+
+Graph3d::Graph3d(vec2 rangeX, vec2 rangeY, vec2 rangeZ, float scale)
+{
+    m_rangeX = rangeX;
+    m_rangeY = rangeY;
+    m_rangeZ = rangeZ;
+    m_scale  = scale;
+    sphere = MeshRenderer::createCubeSphere(5);
+}
+
+void Graph3d::setMesh(uint sizeX, uint sizeZ, bool smooth)
+{
+    meshX = sizeX;
+    meshZ = sizeZ;
+    m_smooth = smooth;
+    if(meshGrid)
+        free(meshGrid);
+
+    meshGrid = (vec3*)malloc(meshX*meshZ*sizeof(vec3));
+    float stepX = (float)10/(meshX-1);
+    float stepZ = (float)10/(meshZ-1);
+    for(uint i=0; i<meshZ; i++)
+        for(uint j=0; j<meshX; j++)
+        {
+            auto& p = meshGrid[i*meshX+j];
+            p.x = j*stepX;
+            p.z = i*stepZ;
+            p.y = 0;
+        }
+    if(m_smooth)
+        mesh = MeshRenderer::createSmoothMeshGrid(meshGrid, meshX, meshZ);
+    else
+        mesh = MeshRenderer::createQuadMeshGrid(meshGrid, meshX, meshZ);
+}
+
+void Graph3d::setPoints(vec3 *_points, uint count)
+{
+    ASSERT(count);
+    nPoints = count;
+    if(points)
+        free(points);
+
+    points = (vec3*)malloc(nPoints*sizeof(vec3));
+    for(uint i=0; i<nPoints; i++)
+    {
+        auto p = _points[i];
+        points[i] = vec3(mapToRange(m_rangeX, {0.f, m_scale}, p.x),
+                         mapToRange(m_rangeY, {0.f, m_scale}, p.y),
+                         mapToRange(m_rangeZ, {0.f, m_scale}, p.z));
+    }
+}
+
+void Graph3d::updateMesh(std::function<float(const vec2&)> func)
+{
     for(uint i=0; i<meshX*meshX; i++)
     {
         auto& p = meshGrid[i];
-        float x1 = mapToRange({0, 10}, {0.f, glm::pi<float>()}, p.x);
-        float x2 = mapToRange({0, 10}, {0.f, glm::pi<float>()}, p.z);
-        p.y = mapToRange({-1,1}, {0, 10}, cos(x1*x2+accum)*cos(2*x1+accum));
+        float x1 = mapToRange({0, m_scale}, m_rangeX, p.x);
+        float x2 = mapToRange({0, m_scale}, m_rangeZ, p.z);
+        p.y = mapToRange({-1,1}, {0, m_scale}, func({x1, x2}));
     }
-
-    graph3d({0,0,0}, points, 1000, false);
+    if(m_smooth)
+        mesh = MeshRenderer::createSmoothMeshGrid(meshGrid, meshX, meshZ);
+    else
+        mesh = MeshRenderer::createQuadMeshGrid(meshGrid, meshX, meshZ);
 }
 
-void TestMLP::graph3d(vec3 pos, vec3 *points, size_t count, bool surface, bool smooth)
+void Graph3d::draw(const vec3 &pos)
 {
+    drawGrid(pos);
+    MeshRenderer::begin();
+    if(meshGrid)
+    {
+        glDisable(GL_CULL_FACE);
+        MeshRenderer::drawMesh(pos, vec3(1.0f), mesh);
+        glEnable(GL_CULL_FACE);
+    }
+    if(points)
+        for(size_t i=0; i<nPoints; i++)
+            MeshRenderer::drawMesh(points[i], vec3(m_scale*0.01f), sphere, {0.2f,0.2f,0.6f,1});
+    MeshRenderer::end();
+}
 
+void Graph3d::drawGrid(const vec3& pos)
+{
     BatchRenderer::begin();
+    float lWidth = m_scale * 0.01f;
     // Draw axes
-    BatchRenderer::drawLine(pos, pos+vec3(10,  0, 0), 0.1f, {0.788, 0.820, 0.851,1});
-    BatchRenderer::drawLine(pos, pos+vec3( 0, 10, 0), 0.1f, {0.788, 0.820, 0.851,1});
-    BatchRenderer::drawLine(pos, pos+vec3( 0,  0,10), 0.1f, {0.788, 0.820, 0.851,1});
+    BatchRenderer::drawLine(pos, pos+m_scale*vec3(1, 0, 0), lWidth, {0.788, 0.820, 0.851,1});
+    BatchRenderer::drawLine(pos, pos+m_scale*vec3(0, 1, 0), lWidth, {0.788, 0.820, 0.851,1});
+    BatchRenderer::drawLine(pos, pos+m_scale*vec3(0, 0, 1), lWidth, {0.788, 0.820, 0.851,1});
 
     // Draw planes
-    BatchRenderer::drawLine(pos+vec3(0, 2, 0), pos+vec3(10, 2, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 4, 0), pos+vec3(10, 4, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 6, 0), pos+vec3(10, 6, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 8, 0), pos+vec3(10, 8, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.2f, 0), pos+m_scale*vec3(1, 0.2f, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.4f, 0), pos+m_scale*vec3(1, 0.4f, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.6f, 0), pos+m_scale*vec3(1, 0.6f, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.8f, 0), pos+m_scale*vec3(1, 0.8f, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
-    BatchRenderer::drawLine(pos+vec3(0, 0, 2), pos+vec3(10, 0, 2), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 4), pos+vec3(10, 0, 4), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 6), pos+vec3(10, 0, 6), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 8), pos+vec3(10, 0, 8), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.2f), pos+m_scale*vec3(1, 0, 0.2f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.4f), pos+m_scale*vec3(1, 0, 0.4f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.6f), pos+m_scale*vec3(1, 0, 0.6f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.8f), pos+m_scale*vec3(1, 0, 0.8f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
-    BatchRenderer::drawLine(pos+vec3(2, 0, 0), pos+vec3(2, 10, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(4, 0, 0), pos+vec3(4, 10, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(6, 0, 0), pos+vec3(6, 10, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(8, 0, 0), pos+vec3(8, 10, 0), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.2f, 0, 0), pos+m_scale*vec3(0.2f, 1, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.4f, 0, 0), pos+m_scale*vec3(0.4f, 1, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.6f, 0, 0), pos+m_scale*vec3(0.6f, 1, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.8f, 0, 0), pos+m_scale*vec3(0.8f, 1, 0), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
-    BatchRenderer::drawLine(pos+vec3(0, 0, 2), pos+vec3(0, 10, 2), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 4), pos+vec3(0, 10, 4), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 6), pos+vec3(0, 10, 6), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 0, 8), pos+vec3(0, 10, 8), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.2f), pos+m_scale*vec3(0, 1, 0.2f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.4f), pos+m_scale*vec3(0, 1, 0.4f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.6f), pos+m_scale*vec3(0, 1, 0.6f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0, 0.8f), pos+m_scale*vec3(0, 1, 0.8f), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
-    BatchRenderer::drawLine(pos+vec3(0, 2, 0), pos+vec3(0, 2, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 4, 0), pos+vec3(0, 4, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 6, 0), pos+vec3(0, 6, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(0, 8, 0), pos+vec3(0, 8, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.2f, 0), pos+m_scale*vec3(0, 0.2f, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.4f, 0), pos+m_scale*vec3(0, 0.4f, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.6f, 0), pos+m_scale*vec3(0, 0.6f, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0, 0.8f, 0), pos+m_scale*vec3(0, 0.8f, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
-    BatchRenderer::drawLine(pos+vec3(2, 0, 0), pos+vec3(2, 0, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(4, 0, 0), pos+vec3(4, 0, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(6, 0, 0), pos+vec3(6, 0, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
-    BatchRenderer::drawLine(pos+vec3(8, 0, 0), pos+vec3(8, 0, 10), 0.01f, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.2f, 0, 0), pos+m_scale*vec3(0.2f, 0, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.4f, 0, 0), pos+m_scale*vec3(0.4f, 0, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.6f, 0, 0), pos+m_scale*vec3(0.6f, 0, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
+    BatchRenderer::drawLine(pos+m_scale*vec3(0.8f, 0, 0), pos+m_scale*vec3(0.8f, 0, 1), lWidth/10, {0.788, 0.820, 0.851, 0.5});
 
     BatchRenderer::end();
-
-
-
-
-    MeshRenderer::begin();
-    // Draw points
-    for(size_t i=0; i<count; i++)
-    {
-        auto p = points[i];
-        MeshRenderer::drawMesh(p, vec3(0.1f), sphere, {0.2f,0.2f,0.6f,1});
-    }
-    glDisable(GL_CULL_FACE);
-    originalMesh = MeshRenderer::createQuadMeshGrid(meshGrid, meshX, meshX);
-    MeshRenderer::drawMesh(vec3(0.f), vec3(1.f), originalMesh);
-    glEnable(GL_CULL_FACE);
-
-    MeshRenderer::end();
 }
--- IPZ_Client/src/Tests/testmlp.h
+++ IPZ_Client/src/Tests/testmlp.h
@@ -1,7 +1,32 @@
 ﻿#pragma once
-
 #include "../Core/application.h"
 #include "../Core/scene.h"
+#include <functional>
+
+class Graph3d
+{
+public:
+    Graph3d(vec2 rangeX, vec2 rangeY, vec2 rangeZ, float scale);
+    void setMesh(uint sizeX, uint sizeZ, bool smooth);
+    void setPoints(vec3* points, uint count);
+    void updateMesh(std::function<float(const vec2&)> func);
+    void draw(const vec3& pos);
+
+private:
+    vec3* meshGrid = nullptr;
+    uint meshX = 0;
+    uint meshZ = 0;
+    vec3* points   = nullptr;
+    uint nPoints   = 0;
+    std::shared_ptr<Mesh> mesh;
+    std::shared_ptr<Mesh> sphere;
+    vec2 m_rangeX;
+    vec2 m_rangeY;
+    vec2 m_rangeZ;
+    float m_scale;
+    bool m_smooth;
+    void drawGrid(const vec3 &pos);
+};
 
 class TestMLP : public Scene
 {
@@ -9,15 +34,13 @@ public:
     TestMLP();
     ~TestMLP(){/*dontcare*/};
     void onUpdate(float dt);
-    void graph3d(vec3 pos, vec3* points, size_t count, bool surface, bool smooth = false);
 
 private:
-    std::shared_ptr<Mesh> sphere;
     vec3* points;
-    vec3* meshGrid;
-    vec4* colorData;
+    uint pointCount = 1000;
     uint meshX = 50;
     float accum = 0;
-    std::shared_ptr<Mesh> originalMesh;
+    vec2 rangeXZ = {0.f, glm::pi<float>()};
+    Graph3d graph = Graph3d(rangeXZ, {-1, 1}, rangeXZ, 10);
 };
 
